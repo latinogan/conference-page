454fa061e4c3326de9fe41e5b5fa149e85c6737e
latinogan <roger.castillo.arce@gmail.com>
2022-04-15 10:54:48 -0500

    add function to the menu hamburguer and voice of costumer dinamicly created with js

--- script.js
+++ script.js
@@ -1,32 +1,85 @@
-const btn = document.querySelector('.icon');
-const icon = btn.querySelector('.fa-bars');
+const btn = document.querySelector('.hamburguer');
+const icon = btn.querySelector('.close');
 const x = document.getElementById('Links');
 let menu = false;
 btn.onclick = function () {
   if (x.style.display === 'block') {
     x.style.display = 'none';
-    icon.classList.replace('fa-times', 'fa-bars');
+    icon.classList.remove('hamburguer');
+    icon.classList.add('close');
   } else {
     x.style.display = 'block';
-    icon.classList.replace('fa-bars', 'fa-times');
   }
 };
 btn.addEventListener('click', () => {
   if (!menu) {
-    icon.classList.replace('fa-bars', 'fa-times');
     menu = true;
   } else {
-    icon.classList.replace('fa-times', 'fa-bars');
+    icon.classList.remove('hamburguer');
+    icon.classList.add('close');
     menu = false;
   }
 });
 x.addEventListener('click', () => {
   if (x.style.display === 'block') {
     x.style.display = 'none';
-    icon.classList.replace('fa-times', 'fa-bars');
     menu = false;
   } else {
     x.style.display = 'flow';
     menu = true;
   }
+});
+
+const client = [
+  {
+    img: './pictures/client1.png" class=person alt="client1',
+    name: 'Jerick Guzman.',
+    city: '.Atlanta',
+    experience: 'treavelers car rentals was a great chiose good costumer service and also they are aware of the costumer was a great expirience ',
+  },
+  {
+    img: './pictures/client2.png" class=person alt="client1',
+    name: 'Jackson.',
+    city: '.New York',
+    experience: 'treavelers car rentals was a great chiose good costumer service and also they are aware of the costumer was a great expirience ',
+  },
+  {
+    img: './pictures/client3.png" class=person alt="client1',
+    name: 'Angela cardenas.',
+    city: '.Miami',
+    experience: 'treavelers car rentals was a great chiose good costumer service and also they are aware of the costumer was a great expirience ',
+  },
+  {
+    img: './pictures/client4.png" class=person alt="client1',
+    name: 'yessica Harold.',
+    city: 'Texas',
+    experience: 'treavelers car rentals was a great chiose good costumer service and also they are aware of the costumer was a great expirience ',
+  },
+  {
+    img: './pictures/client5.png" class=person alt="client1',
+    name: 'Diego Molina.',
+    city: '.Illinoise',
+    experience: 'treavelers car rentals was a great chiose good costumer service and also they are aware of the costumer was a great expirience ',
+  },
+  {
+    img: './pictures/client6.png" class=person alt="client1',
+    name: 'Abraham chantre.',
+    city: '.Ohio',
+    experience: 'treavelers car rentals was a great chiose good costumer service and also they are aware of the costumer was a great expirience ',
+  },
+];
+const container = document.querySelector('.content');
+
+client.forEach((result) => {
+  const asosiado = `
+  
+  <img  src="${result.img}" class="person" alt="client1">
+    <div class="voice">
+    <h3>${result.name}</h3>
+    <h5>${result.city} </h5>
+    <p>${result.experience}    </p>
+    </div>    
+  `;
+  // Append newyly created card element to the container
+  container.innerHTML += asosiado;
 });
